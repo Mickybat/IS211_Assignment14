43ac2ed443a47901aedc571d0aaaa32b0a809dfb
Mickybat <99045830+Mickybat@users.noreply.github.com>
2022-05-05 21:41:28 -0400

    Initial commit

---
+++ recursion.py
@@ -0,0 +1,43 @@
+"""PART NUMBER 1- IMPLEMENT THE FIBONNACI SEQUENCE"""
+
+
+def fibonnaci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonnaci(n-1) + fibonnaci(n-2)
+
+
+print(fibonnaci(15))
+
+"""PART 2- IMPLEMENT EUCLID'S GCD ALGORITHM"""
+
+
+def gcd(a, b):
+    if a == 0:
+        return b
+    return gcd(b % a, a)
+
+
+a = 500
+b = 50
+
+print("gcd(", a, ",", b, ") = ", gcd(a, b))
+
+"""PART 3- STRING COMPARISON"""
+
+
+def compareTo(s1, s2):
+    if len(str(s1)) > len(str(s2)):
+        return 1
+    if len(str(s1)) < len(str(s2)):
+        return -1
+    else:
+        compare(len(str(s1)) == len(str(s2)), len(str(s1)))
+        return 0
+
+
+s1 = 'conversation'
+s2 = 'talking'
+print(compareTo(s1, s2))
+
